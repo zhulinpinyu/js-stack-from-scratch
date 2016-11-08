190198a6efd7901f026ec59d124a6bf6988ff289
zhulinpinyu <mulixiang199016@gmail.com>
2016-11-08 16:47:05 +0800

    immutable js 和 redux的进一步应用

--- src/client/actions/dog-actions.js
+++ src/client/actions/dog-actions.js
@@ -1,6 +1,10 @@
+import { createAction } from 'redux-actions'
+
 export const MAKE_BARK = 'MAKE_BARK'
 
-export const makeBark = () => ({
-  type: MAKE_BARK,
-  payload: true
-})
+// export const makeBark = () => ({
+//   type: MAKE_BARK,
+//   payload: true
+// })
+
+export const makeBark = createAction(MAKE_BARK, () => true)
--- src/client/containers/bark-message.js
+++ src/client/containers/bark-message.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 import Message from '../components/message'
 
 const mapStateToProps = state => ({
-  message: state.dog.hasBarked ? 'Dog has barked!' : 'Dog not bark'
+  message: state.dog.get('hasBarked') ? 'Dog has barked!' : 'Dog not bark'
 })
 
 export default connect(mapStateToProps)(Message)
